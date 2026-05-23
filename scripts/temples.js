92cb886058f381c2e9b4ca360b7f8bf2df66d993
Dieuveille <mabdieuveille@gmail.com>
2026-05-23 16:08:11 +0200

    navbar media query corrected

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,3 +1,16 @@
 document.getElementById("currentyear").textContent = new Date().getFullYear();
 document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
 
+const hamburgerButton = document.querySelector('#menu');
+const navigation = document.querySelector('.nav-links');
+const mainNavigation = document.querySelector('.main-header')
+const headerTitle = document.querySelector('#header-title')
+
+hamburgerButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamburgerButton.classList.toggle('open');
+    headerTitle.classList.toggle('hidden')
+    mainNavigation.classList.toggle('open')
+    //  we need to make the paragraph disappear
+
+});
