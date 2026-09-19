27d8363447cced2844401092b4d4b70c1c2c7556
domme19 <mabdieuveille@gmail.com>
2026-09-19 14:06:41 +0200

    temples.js added

---
+++ scripts/temples.js
@@ -0,0 +1,15 @@
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
+
+const hamburgerButton = document.querySelector('#menu');
+const navigation = document.querySelector('.nav-links');
+const mainNavigation = document.querySelector('.main-header')
+const headerTitle = document.querySelector('#header-title')
+
+hamburgerButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamburgerButton.classList.toggle('open');
+    headerTitle.classList.toggle('hidden')
+    mainNavigation.classList.toggle('open')
+
+});
